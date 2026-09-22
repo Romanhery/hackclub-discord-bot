2bb2daaa31e6685517e5c7a9dfc00f59d020b36b
Roman Sultani <inatlusnamor@gmail.com>
2026-09-22 16:44:01 -0700

    Removed tutorials

--- main.py
+++ main.py
@@ -82,49 +82,5 @@ async def add_Gallery(interaction: discord.Interaction, title:str , link:str , i
             branch="main"
     )
 
-
-
-    @client.tree.command(name="add-tutorial", description="Adds to the tutorials page!",guild=GUILD_ID)
-    #function for adding card to gallery takes in parameters title link and img
-    async def add_Gallery(interaction: discord.Interaction, title:str , link:str , img: str):
-        #sends confirmation message
-        await interaction.response.send_message(f'Added {title} to the gallery!')
-
-        g = Github(auth=auth, lazy=True, api_version="2022-11-28")
-
-        #get the repo
-        repo = g.get_repo("Romanhery/Antelope-Hackclub-Website")
-
-        #get the current content 
-        contents = repo.get_contents("assets/gallery.json", ref="main")
-
-        #decode it into something it can read
-        current_content = contents.decoded_content.decode('utf-8')
-
-        # puts the content into a variable
-        data = json.loads(current_content) if current_content.strip() else []
-
-        #gallery card template
-        new_gallery_card = {
-            "name": title,
-            "link" : link,
-            "image" : img
-        }
-
-        #add the new card
-        data.append(new_gallery_card)
-
-        #parsted content and full finished 
-        updated_text = json.dumps(data, indent=2)
-
-        #update the repo with the data
-        repo.update_file(
-                path=contents.path,
-                message="feat: update gallery",
-                content=updated_text,
-                sha=contents.sha,
-                branch="main"
-        )
-
 #run the discord bot
 client.run(os.getenv("CLIENT_ID"))
